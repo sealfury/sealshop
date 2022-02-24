5c250b08cec36a83ad91601b965432df34d8782d
sealfury <willspecker@gmail.com>
2022-02-24 17:52:11 +0100

    adds ts config settings to dynamic framework config

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,12 +1,22 @@
 const path = require('path')
+const fs = require('fs')
 const merge = require('deepmerge')
 
 function useFrameworkConfig(defaultConfig = {}) {
-  const framework = 'shopify' // dynamically incorporate in future
+  const framework = 'shopify' // dynamically incorporate hardcoded val in future
 
+  // project-specific config
   const fwkNextConfig = require(path.join('../', framework, 'next.config'))
-
   const config = merge(defaultConfig, fwkNextConfig)
+
+  // TS routing config
+  const tsPath = path.join(process.cwd(), 'tsconfig.json')
+  const tsConfig = require(tsPath)
+
+  tsConfig.compilerOptions.paths['@framework'] = [`framework/${framework}`]
+  tsConfig.compilerOptions.paths['@framework/*'] = [`framework/${framework}/*`]
+
+  fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2))
 
   return config
 }
