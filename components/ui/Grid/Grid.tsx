1a4f8b51fd17ec1fdcb2e5b149e6d74e0450179a
sealfury <willspecker@gmail.com>
2022-02-25 18:07:41 +0100

    styles reusable grid component & fixes tailwind/postcss config struct

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,7 +1,8 @@
+import s from './Grid.module.css'
 import { ReactNode } from 'react'
 
 const Grid: React.FC<ReactNode> = ({ children }) => {
-  return <div className='root-grid'>{children}</div>
+  return <div className={s.root}>{children}</div>
 }
 
 export default Grid
