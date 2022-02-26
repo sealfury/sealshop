b6052a3ed8ab4cff1fa9d10863ba7411156c36bf
sealfury <willspecker@gmail.com>
2022-02-26 21:25:37 +0100

    adds marquee style variants for multiple implementations of the component

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,13 +1,21 @@
 import s from './Marquee.module.css'
 import Ticker from 'react-ticker'
+import classNames from 'classnames'
+
+export type MarqueeVariant = 'primary' | 'secondary'
 
 interface MarqueeProps {
   children: React.ReactNode[]
+  variant?: MarqueeVariant
 }
 
-const Marquee: React.FC<MarqueeProps> = ({ children }) => {
+const Marquee: React.FC<MarqueeProps> = ({ children, variant = 'primary' }) => {
+  const rootClass = classNames(s.root, {
+    [s.secondary]: variant === 'secondary',
+  })
+
   return (
-    <div className={s.root}>
+    <div className={rootClass}>
       <Ticker offset={80}>
         {() => <div className={s.marqueeWrapper}>{children}</div>}
       </Ticker>
--- pages/index.tsx
+++ pages/index.tsx
@@ -42,6 +42,11 @@ export default function Home({
           <ProductCard key={product.id} product={product} />
         ))}
       </Grid>
+      <Marquee variant='secondary'>
+        {products.slice(0, 3).map(product => (
+          <ProductCard key={product.id} product={product} variant='slim' />
+        ))}
+      </Marquee>
     </>
   )
 }
