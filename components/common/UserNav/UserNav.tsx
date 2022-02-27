9660a183c25de8af110ff0daf07d538bfa92f103
sealfury <willspecker@gmail.com>
2022-02-27 23:30:46 +0100

    implements item components from shopify standard library in navbar

--- components/common/UserNav/UserNav.tsx
+++ components/common/UserNav/UserNav.tsx
@@ -1,14 +1,19 @@
 import s from './UserNav.module.css'
 import Link from 'next/link'
+import { Bag as Cart, Heart } from '@components/icons'
 
 const UserNav: React.FC = () => {
   return (
     <nav>
       <ul className={s.list}>
-        <li className={s.item}>Cart</li>
+        <li className={s.item}>
+          <Cart />
+        </li>
         <li className={s.item}>
           <Link href='/'>
-            <a>Wishlist</a>
+            <a>
+              <Heart />
+            </a>
           </Link>
         </li>
       </ul>
