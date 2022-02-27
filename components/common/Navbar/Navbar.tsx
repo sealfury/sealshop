477757d7e59621c66245c187bb67e47b9c69fb69
sealfury <willspecker@gmail.com>
2022-02-27 23:02:48 +0100

    adds navbar link elements & styles

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,10 +1,30 @@
+import s from './Navbar.module.css'
+import Link from 'next/link'
 import { Container } from '@components/ui'
 
 const Navbar: React.FC = () => {
   return (
     <Container>
-      <div className='flex flex-row md:py-6'>
-        <h1>- S E A L S H O P -</h1>
+      <div className={s.root}>
+        <div className='flex flex-1 items-center'>
+          <Link href='/'>
+            <a className={s.navLogo}>~~ S E A L S H O P ~~</a>
+          </Link>
+          <nav className='ml-6 space-x-6'>
+            <Link href='/'>
+              <a className={s.navLink}>All Produts</a>
+            </Link>
+            <Link href='/'>
+              <a className={s.navLink}>Clothing</a>
+            </Link>
+            <Link href='/'>
+              <a className={s.navLink}>Accessories</a>
+            </Link>
+            <Link href='/'>
+              <a className={s.navLink}>Artwork</a>
+            </Link>
+          </nav>
+        </div>
       </div>
     </Container>
   )
