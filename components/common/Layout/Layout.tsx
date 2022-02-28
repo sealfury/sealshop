f3a9ac0c9fd32dd589e8f8cca636837740e98ae6
sealfury <willspecker@gmail.com>
2022-02-28 13:21:31 +0100

    creates generic sidebar component (reusable)

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import s from './Layout.module.css'
 import { Footer, Navbar } from '@components/common'
+import { Sidebar } from '@components/ui'
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div className={s.root}>
       <Navbar />
+      <Sidebar>Child Placeholder</Sidebar>
       <main className='fit'>{children}</main>
       <Footer />
     </div>
---
+++ components/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+interface SidebarProps {
+  children: any
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const isOpen = true
+
+  return (
+    <>
+      {isOpen ? (
+        <div className='fixed inset-0 overflow-hidden h-full z-50'>
+          <div className='absolute inset-0 overflow-hidden'>
+            <div
+              className='absolute inset-0 bg-black bg-opacity-50 transition-opacity'
+              onClick={() => {}}
+            />
+            <section className='absolute inset-y-0 right-0 pl-10 max-w-full flex sm:pl-16 outline-none'>
+              <div className='h-full md:w-screen md:max-w-md'>
+                <div className='h-full flex flex-col text-base bg-accents-1 shadow-xl overflow-y-auto'>
+                  {children}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      ) : null}
+    </>
+  )
+}
+
+export default Sidebar
