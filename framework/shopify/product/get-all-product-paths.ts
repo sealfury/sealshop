eded57978bf88bdaf7713efe18682cc4ec07a56e
sealfury <willspecker@gmail.com>
2022-02-28 21:10:38 +0100

    enables server side data fetching for product paths (slugs)

--- framework/shopify/product/get-all-product-paths.ts
+++ framework/shopify/product/get-all-product-paths.ts
@@ -1,18 +1,27 @@
 import { ApiConfig } from '@common/types/api'
 import { Product } from '@common/types/product'
+import { ProductConnection } from '@framework/schema'
+import getAllProductPathsQuery from '@framework/utils/queries/get-all-product-paths'
 
 type ReturnType = {
   products: Pick<Product, 'slug'>[]
 }
 
 const getAllProductPaths = async (config: ApiConfig): Promise<ReturnType> => {
-  return {
-    products: [
-      { slug: 'cool-hat' },
-      { slug: 't-shirt' },
-      { slug: 'lightweight-jacket' },
-    ],
-  }
+  // retrieve slug for path from schema
+  const { data } = await config.fetch<{ products: ProductConnection }>({
+    query: getAllProductPathsQuery,
+    url: config.apiUrl,
+  })
+
+  const products = data.products.edges.map(({ node: { handle } }) => {
+    return {
+      slug: handle,
+    }
+  })
+
+  console.log(products)
+  return { products }
 }
 
 export default getAllProductPaths
