52ece2896dd503409d1ad06a5e52b1f1954f42dd
sealfury <willspecker@gmail.com>
2022-03-02 11:52:58 +0100

    adds basic sliding functionality

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,10 +1,23 @@
 import s from './ProductSlider.module.css'
 import { Children, isValidElement, cloneElement } from 'react'
+import { useKeenSlider } from 'keen-slider/react'
 
 const ProductSlider: React.FC = ({ children }) => {
+  // see keen slider docs for details
+  const [sliderRef, _] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(s) {
+      console.log(`changing to `)
+    },
+  })
+
   return (
     <div className={s.root}>
-      <div className='keen-slider h-full transition-opacity'>
+      <div
+        ref={sliderRef as any}
+        className='keen-slider h-full transition-opacity'
+      >
         {Children.map(children, child => {
           if (isValidElement(child)) {
             // return {
@@ -14,7 +27,11 @@ const ProductSlider: React.FC = ({ children }) => {
             //     className: 'keen-slider__slide',
             //   },
             // }
-            return cloneElement(child, { className: 'keen-slider__slide' })
+            return cloneElement(child, {
+              className: `${
+                child.props.className ? `${child.props.className}` : ''
+              } keen-slider__slide`,
+            })
           }
 
           return child
