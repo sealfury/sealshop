ead3b2f054c53daf6791d757227dc1b1bc28f705
sealfury <willspecker@gmail.com>
2022-03-02 11:25:46 +0100

    begins keen slider intergration for product slider component

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,9 +1,25 @@
 import s from './ProductSlider.module.css'
+import { Children, isValidElement, cloneElement } from 'react'
 
 const ProductSlider: React.FC = ({ children }) => {
   return (
     <div className={s.root}>
-      <div className='h-full transition-opacity'>{children}</div>
+      <div className='keen-slider h-full transition-opacity'>
+        {Children.map(children, child => {
+          if (isValidElement(child)) {
+            // return {
+            //   ...child,
+            //   props: {
+            //     ...child.props,
+            //     className: 'keen-slider__slide',
+            //   },
+            // }
+            return cloneElement(child, { className: 'keen-slider__slide' })
+          }
+
+          return child
+        })}
+      </div>
     </div>
   )
 }
